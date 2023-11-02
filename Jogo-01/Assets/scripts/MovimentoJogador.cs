e6d0d2211523f619363c11c074699da448d30f7a
Tallyson Silva <113393169+Tallyson95@users.noreply.github.com>
2023-11-02 19:46:03 -0300

    movimentos

---
+++ Jogo-01/Assets/scripts/MovimentoJogador.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoJogador : MonoBehaviour
+{
+    private Rigidbody2D rigidbory2D;
+    public float velocidadePersonagem;
+    void Awake(){
+        rigidbory2D = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentoPersonagem();
+    }
+
+    private void MovimentoPersonagem(){ 
+        float MovimentoHorizontal = Input.GetAxis("Horizontal");
+        float eixoX = MovimentoHorizontal * velocidadePersonagem;
+        float eixoy = rigidbory2D.velocity.y;
+
+        rigidbory2D.velocity = new Vector2(eixoX, eixoy);
+        if(MovimentoHorizontal > 0){
+            transform.localScale = new Vector3(1f,1f,1f);
+        }else{
+            if(MovimentoHorizontal < 0){
+                transform.localScale = new Vector3(-1f,1f,1f);
+            }
+        }
+    }
+}

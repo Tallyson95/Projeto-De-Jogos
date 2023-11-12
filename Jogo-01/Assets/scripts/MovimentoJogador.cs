fed9165b211b1b04a6cb5af31ce2a23152cc9dd7
Nix <rpaiva.nicole@gmail.com>
2023-11-12 20:22:28 -0300

    testando

--- Jogo-01/Assets/scripts/MovimentoJogador.cs
+++ Jogo-01/Assets/scripts/MovimentoJogador.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#testandotestandotestando
+
 public class MovimentoJogador : MonoBehaviour
 {
     private Rigidbody2D rigidbory2D;
